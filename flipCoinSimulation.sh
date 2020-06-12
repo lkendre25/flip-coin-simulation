a5ff6ea7f5c756618766816506a04b50c26f64bf
lkendre25 <lkendre5652@gmail.com>
2020-06-13 00:54:11 +0530

    l

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 h=0
 t=0
-read -p "Enter the number how much time you want to flip coin" c
-for ((i=0;i< $c;i++))
+read -p "Press Enter Key to Play !! "
+#for ((i=0;i< $c;i++))
+while [ true ]
 do
 	rs=$(( 1 + RANDOM % 2 ))
 	if [ $rs -eq 1 ]
 	then
+		sleep 1
 		echo "Head"
 		let h++
+		if [ $h -eq 21 ];then
+			echo " Head Win "
+			break
+		fi
 	else
+		sleep 1
 		echo "Tail"
 		let t++
+		if [ $t -eq 21 ];then
+			echo " Tail Win"
+			break
+		fi
 	fi
 done
 echo "Head count is : $h "

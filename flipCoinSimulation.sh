1ad5caffe78ac15f7a645633b2d1d61704693c6d
lkendre25 <lkendre5652@gmail.com>
2020-06-13 00:37:42 +0530

    Flipped Coin count

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
-
-coin=$(( 1 RANDOM % 2 ))
-
-if [ $coin -eq 1 ]
-then
-	echo " Head "
-else
-	echo " Tail "
-fi
+h=0
+t=0
+read -p "Enter the number how much time you want to flip coin" c
+for ((i=0;i< $c;i++))
+do
+	rs=$(( 1 + RANDOM % 2 ))
+	if [ $rs -eq 1 ]
+	then
+		echo "Head"
+		let h++
+	else
+		echo "Tail"
+		let t++
+	fi
+done
+echo "Head count is : $h "
+echo "Tail Count is : $t "
